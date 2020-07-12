ce45ce2e97205e4b59838cd6a5d9aaf3f2e57219
YeetsyougottaEEEEE <67150450+YeetsyougottaEEEEE@users.noreply.github.com>
2020-07-12 18:53:01 +0530

    Add files via upload

--- crumpledpaper.js
+++ crumpledpaper.js
@@ -1,29 +1,38 @@
-class paper {
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
 
-constructor(x,y,radius){
-var options={
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
 
-    'isStatic':false,
-    'restitution':0.3,
-    'friction':0.5,
-    'density':1.2
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
-this.x=x;
-this.y=y;
-this.radius=radius;
-this.body=Bodies.circle(x,y,radius,options);
-World.add(world,this.body);
-}
-
-display(){
-var pos = this.body.posotion;
-var angle = this.body.angle;
-push();
-translate(pos.x,pos.y);
-rotate(angle);
-elipseMode(RADIUS);
-fill(255);
-elipse(0,0,this.angle);
-pop;
-
-}}
--- sketch.js
+++ sketch.js
@@ -1,40 +1,56 @@
-var ball, side1, side2, side3;
+var ball, ground1, didin, chain;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+const Render = Matter.Render;
 
-function preload()
-{
-	
-}
+
 
 function setup() {
 	createCanvas(1200, 600);
-	side1=createSprite(900,390,100,20);
-	side2=createSprite(800,360,50,20);
-	side3=createSprite(1000,360,50,20);
-    ball = new paper(100,590,5);
-	
+  engine = Engine.create();
+  world = engine.world;
+  
+  ball = new paper(1000,100,20)
+  ground1 = new ground(600,300,20,1200);
+  didin = new dustbin(1000,590);
+  chain = new slingshot(ball.body,{x:900,y:500});
 
-	
-	
-	
-	Engine.run(engine);
-    engine = Engine.create();
-	world = engine.world;
+  var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
  
+ 
+  
+	Engine.run(engine);
+//	Render.run(render);
+	
 }
 
 
 function draw() {
   rectMode(CENTER);
+  background(230);
+
   ball.display();
-  background(0);
-  
-  drawSprites();
- 
+  ground1.display();
+  didin.display();
 }
 
+function mouseDragged(){
+  Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+  chain.fly();
+}
 
 
